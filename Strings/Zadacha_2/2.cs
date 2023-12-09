00cac9d04bc84decbda324225844805f82edb198
YarkoffYuriy@gmail.com <YarkoffYuriy@gmail.com>
2023-12-09 17:59:32 +0300

    Zadacha_2

---
+++ Strings/Zadacha_2/2.cs
@@ -0,0 +1,6 @@
+﻿//Задайте строку, содержащую латинские буквы в обоих регистрах. 
+//Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+
+string Stroka = "ASdfasASdasda";
+string result = Stroka.ToLower();
+System.Console.WriteLine(result);
