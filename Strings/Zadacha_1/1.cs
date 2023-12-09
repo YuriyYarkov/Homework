f17a36991f4a0a4d7d70a2706076094422b61403
YarkoffYuriy@gmail.com <YarkoffYuriy@gmail.com>
2023-12-09 17:53:45 +0300

    Zadacha_1

---
+++ Strings/Zadacha_1/1.cs
@@ -0,0 +1,15 @@
+﻿//Задайте двумерный массив символов (тип char [,]). 
+//Создать строку из символов этого массива.
+
+
+string GetStringFromCharArray(char [] chars){
+    string res = "";
+    foreach (char elem in chars){
+        res += elem;
+    }
+    return res;
+}
+
+char [] chars = {'a','d','c','b'};
+string str = GetStringFromCharArray(chars);
+Console.WriteLine(str);
