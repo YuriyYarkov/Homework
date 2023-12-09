c3f570454a9bf894afad199f91a67f3846440893
YarkoffYuriy@gmail.com <YarkoffYuriy@gmail.com>
2023-12-09 18:03:43 +0300

    Zadacha_3

---
+++ Strings/Zadacha_3/3.cs
@@ -0,0 +1,31 @@
+﻿//Задайте произвольную строку. Выясните, является ли она палиндромом.
+string FilterString(string str)
+{
+    string res = str.ToLower();
+    string count = "";
+    foreach (char elem in res)
+    {
+        if (elem != ' ')
+        {
+            count += elem;
+        }
+    }
+    return count;
+}
+
+string IsPalindrome(string str)
+{
+    for (int i = 0; i < str.Length / 2; i++)
+    {
+        if (str[i] != str[str.Length - i - 1])
+        {
+            return ("String is not palindrome");
+        }
+    }
+    return ("String is palindrome");
+}
+
+
+string str = "zabaz";
+string res = FilterString(str);
+Console.WriteLine(IsPalindrome(res));
