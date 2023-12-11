04099f4f3a3f76af4cfcd4ee0d74d8b8dfcc08d5
YarkoffYuriy@gmail.com <YarkoffYuriy@gmail.com>
2023-12-11 13:03:56 +0300

    Zadacha_3

---
+++ Recursion/Zadacha_3/3.cs
@@ -0,0 +1,29 @@
+﻿// Задайте произвольный массив. 
+// Выведете его элементы, начиная с конца. 
+// Использовать рекурсию, не использовать циклы.
+
+int[] array = { 2, 6, 12, 4, 67, 33, 17, 88, 11, 4 };
+
+Console.WriteLine("Исходный массив:");
+PrintArray(array, 0);
+
+Console.WriteLine("\nПеревернутый массив:");
+PrintArrayReverse(array, array.Length - 1);
+
+void PrintArray(int[] array, int i)
+{
+    if (i < array.Length)
+    {
+        Console.Write($"{array[i]} ");
+        PrintArray(array, i+1);
+    }
+}
+
+void PrintArrayReverse(int[] array, int i)
+{
+    if (i >= 0)
+    {
+        Console.Write($"{array[i]} ");
+        PrintArrayReverse(array, i-1);
+    }
+}
